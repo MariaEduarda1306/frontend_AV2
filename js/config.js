ebcc10b5092b323f4dfadbd6e9ee62de2f315879
Maria Eduarda <160027727+MariaEduarda1306@users.noreply.github.com>
2026-06-03 15:06:39 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = "https://dispose-vista-sarcastic.ngrok-free.dev";
+
+const API_ENDPOINTS = {
+  vagas: "/vagas",
+  health: "/",
+  candidatos: "/candidatos",
+  candidatura: "/inscricoes",
+  chat: "/chat"
+};
