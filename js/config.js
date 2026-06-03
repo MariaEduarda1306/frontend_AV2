d89172295518905274749ef4dd188818b1821b63
Maria Eduarda <160027727+MariaEduarda1306@users.noreply.github.com>
2026-06-03 11:48:29 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = "https://dispose-vista-sarcastic.ngrok-free.dev";
-
-const API_ENDPOINTS = {
-  vagas: "/vagas",
-  health: "/",
-  candidatos: "/candidatos",
-  candidatura: "/inscricoes"
-};
