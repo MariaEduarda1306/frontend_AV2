e287ac9862bd4ed7a39ca48dfbbb1d96f4190049
Maria Eduarda <160027727+MariaEduarda1306@users.noreply.github.com>
2026-06-03 15:01:58 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "https://panoramic-figure-mushroom.ngrok-free.dev";
-
-const API_ENDPOINTS = {
-  vagas: "/vagas",
-  health: "/",
-  candidatos: "/candidatos",
-  candidatura: "/inscricoes",
-  chat: "/chat"
-};
