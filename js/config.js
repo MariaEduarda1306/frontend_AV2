70ad1d2402414a961acb89ef91dfed7913d2027c
Maria Eduarda <160027727+MariaEduarda1306@users.noreply.github.com>
2026-06-03 08:49:01 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
-const API_BASE_URL = "https://SEU-LINK-NGROK.ngrok-free.app";
+const API_BASE_URL = "https://dispose-vista-sarcastic.ngrok-free.dev";
 
 const API_ENDPOINTS = {
   vagas: "/vagas",
   health: "/",
+  candidatos: "/candidatos",
   candidatura: "/inscricoes"
 };
